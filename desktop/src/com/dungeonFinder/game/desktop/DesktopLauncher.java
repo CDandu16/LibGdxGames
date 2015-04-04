cd0e6c1e12c2eb46571204167fda46d12b4b1d01
CDandu16 <chaitudandu@gmail.com>
2015-04-04 00:02:51 -0400

    Project
    My dungeon game

---
+++ desktop/src/com/dungeonFinder/game/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.dungeonFinder.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dungeonFinder.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = MyGdxGame.HEIGHT;
+		config.width = MyGdxGame.WIDTH;
+		config.title = "Dungeon Crawlers";
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
